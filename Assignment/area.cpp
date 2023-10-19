22658f7ffc6f87a0e7d5713219974f710d233937
frontenddeveloperpraveen <thisiskrpraveen@gmail.com>
2023-10-19 12:37:55 +0530

    Respo Add

---
+++ Assignment/area.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+
+using namespace std;
+
+double cyclinder_volume(double rad, double height);
+
+double circle_area(double a);
+
+void area_circle();
+
+void volume_cylinder();
+
+const double pi = 3.14159;
+
+
+int main(){
+
+    area_circle();
+    volume_cylinder();
+    return 0;
+
+
+
+}
+
+
+
+
+double cyclinder_volume(double rad, double h){
+
+    return voume
+}
---
+++ pre-post-inc-logic.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+
+    int a = 5;
+
+    int ans;
+
+    a = a++;
+
+    cout<<" A :"<<a;
+
+
+
+
+};
